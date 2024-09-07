4a7fc3ccf0e8c281f6f9efe1950defd991967af3
KaNdYKuriyal <kuriyaldivyanshu2003@gmail.com>
2024-09-07 11:19:22 +0530

    add showmore functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,8 @@ export default function DashPosts() {
 
   const {currentUser} = useSelector((state)=>state.user);  
   const [userPosts , setUserPosts] = useState([]);
+  const [showMore , setShowMore] = useState(true);
+
   console.log(userPosts);
   useEffect(()=>{
     const fetchPosts = async()=>{
@@ -15,6 +17,9 @@ export default function DashPosts() {
         const data = await res.json();
         if(res.ok){
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       }
       catch(error){
@@ -25,6 +30,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   } , [currentUser._id])
+
+
+  const handleShowMore = async()=>{
+    const startIndex = userPosts.length;
+    try{
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev)=>[...prev , ...data.posts])
+        if(data.posts.length < 9 ){
+          setShowMore(false);
+        }
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
   
   
   return (
@@ -95,6 +118,13 @@ export default function DashPosts() {
             ))}
 
           </Table>
+          {
+            showMore && (
+              <button className='w-full text-teal-500 self-center text-sm py-7' onClick={handleShowMore}>
+                Show more
+              </button>
+            )
+          }
         </>
       ):( 
         <p> You have no posts yet</p>
