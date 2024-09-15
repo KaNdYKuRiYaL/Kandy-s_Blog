8f75301895cde002a784953a35ad1a81b7e556f6
KaNdYKuriyal <kuriyaldivyanshu2003@gmail.com>
2024-09-16 04:42:43 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 
 mongoose
@@ -28,10 +28,18 @@ app.listen(3000 , ()=>{
     console.log('server is running on port 3000')
 });
 
+const __dirname = path.resolve();
+
 app.use('/api/user' , userRoutes);
 app.use('/api/auth' , authRoutes);
 app.use('/api/post' , postRoutes);
 app.use('/api/comment' , commentRoutes);
+
+app.use(express.static(path.join(__dirname , '/client/dist')));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname , 'client', 'dist' , 'index.html'));
+});
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
